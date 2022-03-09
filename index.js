f2931e8289273bfbe870aca564098299fed937d6
bajoescience <afiliateejoseph@gmail.com>
2022-03-09 13:26:06 +0100

    step3

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 })
 
+
 // Get the number of entries from the phonebook
 app.get('/info', (req, res) => {
     let datenow = new Date();
@@ -38,6 +39,26 @@ app.get('/info', (req, res) => {
     res.send(`<div><p>Phonebook has info for ${persons.length} people</p> <p>${datenow}</p></div>`)
     res.send()
 })
+
+
+// Get a single resource through it's id
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(p => p.id == id);
+    // Handle if person does not exist
+    if (!person) {
+        return res.status(404).json({error: `Sorry, person ${id} does not exist`});
+    }
+    res.json(person);
+})
+
+// Enable the deletion of a resource through id
+app.delete('api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(p => p.id != id);
+    res.status(204).send(`<h2>Person ${id} has succesfully been deleted</h2>`);
+})
+
 
 // Set app to listen on port 3001
 const PORT = 3001;
